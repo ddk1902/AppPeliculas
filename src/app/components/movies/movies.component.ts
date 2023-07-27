25dd19bfeaeb1d59653b7c7f1c204f2eadaf5467
ddk1902 <gomezdiego1902@gmail.com>
2023-07-27 10:55:36 -0400

    Comentarios

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -12,6 +12,11 @@ export class MoviesComponent implements OnInit
 
   ngOnInit(): void { }
 
+
+  // Creamos una función llamada getMovies para poder listar todas las peliculas a través de los términos de búsqueda
+  // Definimos el parámetro searchTerms para los términos de búsqueda
+  // A través de un observable nos subscribimos a los datos que trae la búsqueda
+
   getMovies(searchTerms:string){
     this.movieService.getMovies(searchTerms).subscribe(data => {
       console.log(data);
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,7 @@
+// Importamos httpClient para poder trabajar con peticiones http
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+//A través de la librería rxjs podemos trabajar con Observables
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,10 +9,15 @@ import { Observable } from 'rxjs';
 })
 export class MovieService {
 //ApiKey 69fe1a90
+
+
+//Definimos una variable para definir la url de donde consumimos la API
 private Api_Url: string = 'http://www.omdbapi.com/?apikey=69fe1a90';
 
   constructor(private http:HttpClient) { }
 
+   // Aquí llamamos a la función getMovies que tenemos en el component con los términos de búsqueda
+   //Le decimos al o¡bservable con <any> que nos puede traer todo tipo de resultados
     getMovies(searchTerm : string): Observable<any> {
       return this.http.get(`${this.Api_Url}&s=${searchTerm }`);
      
